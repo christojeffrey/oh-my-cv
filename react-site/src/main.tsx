15891676e639e909d11b44988d24c30c59e9bfcf
Christopher Jeffrey <christopherjeffrey@gmail.com>
2026-01-23 21:59:21 +0700

    Restore router in main.tsx - now shows full Oh My CV app

--- react-site/src/main.tsx
+++ react-site/src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+import './index.css'
 
-const rootElement = document.getElementById('root')
-if (!rootElement) {
-  console.error('Root element not found')
-} else {
-  console.log('Root element found, rendering React')
-  createRoot(rootElement).render(
-    <StrictMode>
-      <div>React is working! Hello World</div>
-    </StrictMode>
-  )
-}
+const router = createRouter({ routeTree })
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
