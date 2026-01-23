2268e228aa4ed125b4b9bed62ce92ab3c0b6b34c
Christopher Jeffrey <christopherjeffrey@gmail.com>
2026-01-23 21:51:47 +0700

    Simplify root route to debug empty screen further

--- react-site/src/routes/__root.tsx
+++ react-site/src/routes/__root.tsx
@@ -1,15 +1,10 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { Header } from '@/components/shared/Header'
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="min-h-screen bg-background">
-      <Header />
-      <main className="container mx-auto px-4 py-8">
-        <Outlet />
-      </main>
-      <TanStackRouterDevtools />
+    <div>
+      Root loaded!
+      <Outlet />
     </div>
   ),
 })
