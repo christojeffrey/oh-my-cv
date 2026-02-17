6f486ecf5f79fa799dbcd4c1848bcca741ad1ac2
Christopher Jeffrey <christopherjeffrey@gmail.com>
2026-02-17 10:24:34 +0700

    chore: fix convex auth

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -2,7 +2,7 @@ export default {
     providers: [
         {
             domain: process.env.CLERK_JWT_ISSUER_DOMAIN,
-            issuer: process.env.CLERK_JWT_ISSUER_DOMAIN,
+            applicationID: "convex",
         },
     ],
 };
